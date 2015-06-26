7cf4371719e2d954a8db498a3d81e4bbcc7a37fc
House.Lee <house.lee@soarnix.com>
2015-06-26 11:31:27 -0600

    add app and resource

---
+++ CoAPNonIP/CoAPNonIP.Core/LibCoAPNonIP/Core/App.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LibCoAPNonIP {
+    public class App {
+        public App() {
+        }
+
+
+    }
+}
+
---
+++ CoAPNonIP/CoAPNonIP.Core/LibCoAPNonIP/Core/Resource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using LibCoAPNonIP.CoAPMsg;
+using LibCoAPNonIP.Network;
+
+namespace LibCoAPNonIP {
+    public delegate void ResourceHandler(Device sender, CoAPRequest request);
+    public class Resource {
+        public Resource(string Name , ResourceHandler Handler) {
+            rr_name = Name;
+            rr_handler = Handler;
+        }
+
+        public ResourceHandler GetHandler() {
+            return rr_handler;
+        }
+
+        public string GetName() {
+            return rr_name;
+        }
+
+        private string rr_name;
+        private ResourceHandler rr_handler;
+    }
+}
+
